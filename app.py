92e20d5270e02614434a9e46d539858e1d63e5b8
LucioJamieAI <47329590+LucioJamieAI@users.noreply.github.com>
2024-07-19 13:32:43 +0100

    Basic Functionality

---
+++ app.py
@@ -0,0 +1,7 @@
+from bot import
+
+
+async def main():
+
+
+if __name__ == '__main__':
--- bot.py
+++ bot.py
@@ -1,27 +1,58 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+import requests
+import os
+from dotenv import load_dotenv
+import pprint
+import json
+import asyncio
 
-import time
+load_dotenv('.env')
+api_key = os.getenv('API_KEY')
+url = "https://places.googleapis.com/v1/places:searchText"
+header = {
+    'X-Goog-Api-Key': api_key,
+    'X-Goog-FieldMask': 'places.displayName,places.name,places.id,places.websiteUri,places.nationalPhoneNumber,places.rating,nextPageToken',
+    'Content-Type': 'application/json'
+}
 
 
-def client_scraper():
-    driver = webdriver.Chrome()
-    base_url = "https://www.google.com/maps/search/business+near+me"
+async def get_clients():
+    global payload
+
+    payload = {
+        'textQuery': 'Restaurants',
+        'locationBias': {
+            'circle': {
+                'center': {'latitude': 51.5964, 'longitude': 0.0349},
+                'radius': 50000.0
+            }
+        },
+        'pageSize': 100
+    }
+
+    rsp = requests.post(url, headers=header, data=json.dumps(payload))
+
+    return rsp.json()
 
 
-    industries = ['restaurants', 'dental+practices']
-    wait = WebDriverWait(driver, timeout=10)
-    for industry in industries:
-        driver.get(base_url.replace('business', industry))
+async def get_next_page():
+    places = await get_clients()
+    if places['nextPageToken']:
+        payload['pageToken'] = places['nextPageToken']
 
-        try:
-            accept_btn = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button")))
-            accept_btn.click()
-        except:
-            pass
+        rsp = requests.post(url, headers=header, data=json.dumps(payload))
+        print(json.dumps(rsp.json(), indent=4))
 
-        time.sleep(9999999)
+        return rsp.json()
 
-    driver.quit()
+    else:
+        print('No more pages to fetch')
+        return None
+
+
+# places = get_clients()
+# print(json.dumps(places, indent=4))
+
+
+if __name__ == '__main__':
+    asyncio.run(get_next_page())
+
